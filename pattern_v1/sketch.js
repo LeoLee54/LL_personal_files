c7629f345bf7f2cd6838ec374f7280c152f0ba40
LeoLee54 <guang19900912@gmail.com>
2020-12-08 20:46:11 -0500

    pattern_v1

--- pattern_v1/sketch.js
+++ pattern_v1/sketch.js
@@ -6,35 +6,94 @@ https://editor.p5js.org/owenroberts/sketches/ELfjvaock
 
 // https://p5js.org/zh-Hans/examples/form-star.html
 
-var r = 255, g = 240, b = 180;
-var speed = 1;
+var r = 237, g = 224, b = 52;
+var speed = 2;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
   noStroke();
 }
 
 function draw() {
-  background(220);
+  background('skyblue');
+
+  noStroke()
+  fill('#5DAF32');
+  triangle(0,400,140,152,280,400);
   
+  noStroke()
+  fill(245,248,240);
+quad(100,220,140,150,180,220,140,190);
+
+noStroke()
+  fill('#5DAF32');
+  triangle(300,400,440,152,580,400);
+  
+  noStroke()
+  fill(245,248,240);
+quad(400,220,440,150,480,220,440,190);
+  
+noStroke();
+fill('white')
+  
+  ellipse(250,50,60,50);
+  ellipse(280,40,60,50);
+  ellipse(330,50,60,50);
+  ellipse(270,70,60,50);
+  ellipse(310,65,60,50);
+
   fill(r, g, b);
-  star(200, 200, 40, 100, 5);
+  star(50, 50, 42, 20, 5);
   
   r += speed;
   g += speed;
   b += speed;
   
-  if (g > 255 || g < 240) {
+  if (g > 255 || g < 224) {
+    speed *= -1;
+  }
+
+   fill(r, g, b);
+  star(150, 50, 42, 20, 5);
+  
+  r += speed;
+  g += speed;
+  b += speed;
+  
+  if (g > 255 || g < 224) {
+    speed *= -1;
+  }
+
+   fill(r, g, b);
+  star(450, 50, 42, 20, 5);
+  
+  r += speed;
+  g += speed;
+  b += speed;
+  
+  if (g > 255 || g < 224) {
     speed *= -1;
   }
   
+
+   fill(r, g, b);
+  star(550, 50, 42, 20, 5);
+  
+  r += speed;
+  g += speed;
+  b += speed;
+  
+  if (g > 255 || g < 224) {
+    speed *= -1;
+  }
+
 }
 
 function star(x, y, radius1, radius2, npoints) {
   let angle = TWO_PI / npoints;
   let halfAngle = angle / 2.0;
   beginShape();
-  for (let a = 1; a < TWO_PI; a += angle) {
+  for (let a = -1; a < TWO_PI; a += angle) {
     let sx = x + cos(a) * radius2;
     let sy = y + sin(a) * radius2;
     vertex(sx, sy);
