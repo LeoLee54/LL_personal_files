27800cd1b0e2ed75d5b3ff97a7762e48ad81fe3a
LeoLee54 <guang19900912@gmail.com>
2020-12-17 12:17:18 -0500

    LL

--- pattern_v2/sketch.js
+++ pattern_v2/sketch.js
@@ -17,31 +17,21 @@ function pattern() {
 
   let w = random(20, 50);
   let h = w;
-  let vertical = random(height/2, height + w);
+  let horizon = random(height/2, height + w);
 
   for (let x = 0; x <= width + w; x += w) { // columns
     for (let y = 0; y <= height + h; y += h) { // rows
-      
 
-      let r, g, b;
-      if (x < vertical) {
-        // more blue
-        r = random(255);
-        g = random(255);
-        b = random(255);
-      } else {
-        // more green
-        r = random(255);
-        g = random(255);
-        b = random(255);
-      }
+let r = random(255);
+let g = random(255);
+let b = random(255);
 
-      fill(r, g, b, 70);
+      fill(r, g, b, 60);
       noStroke();
 
       ellipse(
         x + random(10, 40), 
-        y + random(10, 60), 
+        y + random(20, 60), 
         w + random(40)
       );
       
